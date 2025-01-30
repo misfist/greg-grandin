ca087bb4a8fa32dd1aa9bc6a0e101ec9387ce994
pea@misfist.com <pea@Peas-Mac-Mini-m4.local>
2025-01-30 14:33:56 -0500

    Register block template for book

--- inc/blocks.php
+++ inc/blocks.php
@@ -110,3 +110,163 @@ function publication_date_binding(): string {
 function copyright_binding(): string {
 	return sprintf( '&copy; %s', date( 'Y' ) );
 }
+/**
+ * Register block template for book post type.
+ *
+ * @link https://developer.wordpress.org/reference/hooks/register_post_type_post_type_args/
+ *
+ * @param  array  $args
+ * @param  string $post_type
+ * @return array $args
+ */
+function register_template( array $args ): array {
+	$template = array(
+		array(
+			'core/paragraph',
+			array(
+				'placeholder' => __( 'Add subtitle...', 'greg-grandin' ),
+				'className'   => 'is-style-subtitle',
+				'metadata'    => array(
+					'bindings' => array(
+						'content' => array(
+							'source' => 'core/post-meta',
+							'args'   => array( 'key' => 'subtitle' ),
+						),
+					),
+				),
+			),
+		),
+		array(
+			'core/group',
+			array(
+				'metadata'  => array( 'name' => 'Book Metadata' ),
+				'className' => 'is-style-section-1',
+				'layout'    => array( 'type' => 'default' ),
+			),
+			array(
+				array(
+					'core/paragraph',
+					array(
+						'placeholder' => __( 'Add publication date...', 'greg-grandin' ),
+						'className'   => 'is-style-postmeta',
+						'metadata'    => array(
+							'name'     => __( 'Publication Date', 'greg-grandin' ),
+							'bindings' => array(
+								'content' => array(
+									'source' => 'greg-grandin/publication-date',
+								),
+							),
+						),
+						'fontSize'    => 'small',
+					),
+				),
+				array(
+					'core/paragraph',
+					array(
+						'placeholder' => __( 'Add publisher...', 'greg-grandin' ),
+						'className'   => 'is-style-postmeta',
+						'metadata'    => array(
+							'name'     => __( 'Publisher', 'greg-grandin' ),
+							'bindings' => array(
+								'content' => array(
+									'source' => 'core/post-meta',
+									'args'   => array( 'key' => 'publisher' ),
+								),
+							),
+						),
+						'fontSize'    => 'small',
+					),
+				),
+				array(
+					'core/paragraph',
+					array(
+						'placeholder' => __( 'Add ISBN...', 'greg-grandin' ),
+						'className'   => 'is-style-postmeta',
+						'metadata'    => array(
+							'name'     => __( 'ISBN', 'greg-grandin' ),
+							'bindings' => array(
+								'content' => array(
+									'source' => 'core/post-meta',
+									'args'   => array( 'key' => 'isbn' ),
+								),
+							),
+						),
+						'fontSize'    => 'small',
+					),
+				),
+
+			),
+		),
+		array(
+			'core/buttons',
+			array(
+				'className' => 'is-style-buy-buttons',
+				'metadata'  => array(
+					'name' => __( 'Buy Buttons', 'greg-grandin' ),
+				),
+			),
+			array(
+				array(
+					'core/button',
+					array(
+						'placeholder' => __( 'Add buy button text and URL...', 'greg-grandin' ),
+						'className'   => 'is-style-button-text',
+						'text'        => esc_html__( 'Buy', 'greg-grandin' ),
+						'url'         => '',
+					),
+				),
+				array(
+					'core/button',
+					array(
+						'placeholder' => __( 'Add buy button text and URL...', 'greg-grandin' ),
+						'className'   => 'is-style-button-text',
+						'text'        => esc_html__( 'Buy', 'greg-grandin' ),
+						'url'         => '',
+					),
+				),
+			),
+		),
+		array(
+			'core/separator',
+			array(),
+			array(),
+		),
+		array(
+			'core/paragraph',
+			array(
+				'placeholder' => __( 'Add description...', 'greg-grandin' ),
+			),
+		),
+		array(
+			'core/separator',
+			array(),
+			array(),
+		),
+		array(
+			'core/heading',
+			array(
+				'content'   => __( 'Reviews', 'greg-grandin' ),
+				'className' => 'is-style-text-subtitle',
+				'level'     => 2,
+			),
+			array(),
+		),
+		array(
+			'core/quote',
+			array(
+				'placeholder' => __( 'Add reviews...', 'greg-grandin' ),
+			),
+			array(
+				array(
+					'core/paragraph',
+					array(
+						'placeholder' => __( 'Add review text and citation...', 'greg-grandin' ),
+					),
+				),
+			),
+		),
+	);
+	$args['template'] = $template;
+	return $args;
+}
+add_action( 'register_book_post_type_args', __NAMESPACE__ . '\register_template' );
