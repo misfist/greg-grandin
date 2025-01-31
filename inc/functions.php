d3fe0e6ef82852c0a9c9267e054f527fc5ee3d70
pea@misfist.com <pea@Peas-Mac-Mini-m4.local>
2025-01-31 00:06:23 -0500

    Custom functions

--- inc/functions.php
+++ inc/functions.php
@@ -8,3 +8,30 @@
  */
 
 namespace Greg_Grandin;
+
+/**
+ * Get the subtitle as a link
+ *
+ * @return string
+ */
+function get_subtitle(): string {
+	global $post;
+	$post_id  = get_the_ID();
+	$subtitle = get_post_meta( $post_id, 'subtitle', true );
+	if ( $subtitle ) {
+		return sprintf( '<a href="%s" data-type="book" data-id="%d" rel="bookmark">%s</a>', get_the_permalink( $post_id ), $post_id, esc_html( $subtitle ) );
+	}
+	return '';
+}
+
+/**
+ * Render the subtitle as a link.
+ *
+ * @return void
+ */
+function render_subtitle() {
+	$subtitle = get_subtitle();
+	if ( $subtitle ) {
+		echo $subtitle;
+	}
+}
--- inc/hooks.php
+++ inc/hooks.php
@@ -8,3 +8,15 @@
  */
 
 namespace Greg_Grandin;
+
+function append_subtitle( $title, $post_id ): string {
+	$title = (string) $title;
+	if ( 'book' === get_post_type( $post_id ) && ! is_admin() ) {
+		$subtitle = get_post_meta( $post_id, 'subtitle', true );
+		if ( $subtitle ) {
+			$title = sprintf( '%s <span class="separator">-</span> %s', $title, esc_html( $subtitle ) );
+		}
+	}
+	return $title;
+}
+// add_filter( 'the_title', __NAMESPACE__ . '\append_subtitle', 10, 2 );
