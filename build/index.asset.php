cb72561d76d3c19d92792df1ffa745485cdac5f7
pea@misfist.com <pea@Peas-Mac-Mini-m4.local>
2025-02-07 14:11:13 -0500

    Update styles

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'bad83febfcd196978618');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '71592265e1c53782dcda');
