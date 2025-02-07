7b8fce7c7deef09c8407430cd48aaccff9a31bc0
pea@misfist.com <pea@Peas-Mac-Mini-m4.local>
2025-02-07 02:39:59 -0500

    Load buttons extracted from buy-buttons block

--- patterns/buy-buttons.php
+++ patterns/buy-buttons.php
@@ -5,20 +5,8 @@
  * Categories: Buttons
  * Inserter: false
  */
+use function Greg_Grandin\display_book_buttons;
 ?>
 
-<!-- wp:site-functionality/buy-buttons -->
-<div class="wp-block-site-functionality-buy-buttons">
-	<!-- wp:buttons {"className":"buy-buttons","layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap","orientation":"horizontal"}} -->
-	<div class="wp-block-buttons buy-buttons">
-		<!-- wp:button {"className":"buy-button","placeholder":"Add Buy Button...","url":"#","linkTarget":"_blank" } -->
-		<div class="wp-block-button buy-button"><a class="wp-block-button__link wp-element-button"></a></div>
-		<!-- /wp:button -->
-
-		<!-- wp:button {"className":"buy-button","placeholder":"Add Buy Button...","url":"#","linkTarget":"_blank" } -->
-		<div class="wp-block-button buy-button"><a class="wp-block-button__link wp-element-button"></a></div>
-		<!-- /wp:button -->
-	</div>
-	<!-- /wp:buttons -->
-</div>
-<!-- /wp:site-functionality/buy-buttons -->
+<?php
+display_book_buttons();
