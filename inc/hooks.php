da8c508aebfadd1d84069c4e69b7419ed075d61b
pea@misfist.com <pea@Peas-Mac-Mini-m4.local>
2025-02-07 02:37:55 -0500

    Only display manual excerpt

--- inc/hooks.php
+++ inc/hooks.php
@@ -60,3 +60,18 @@ function remove_book_blocks( $block_content, $block ) {
 // add_filter( 'render_block', __NAMESPACE__ . '\remove_book_blocks', 10, 2 );
 add_filter( 'render_block_core/post-content', __NAMESPACE__ . '\remove_book_blocks', 10, 2 );
 
+/**
+ * Only display manual post excerpt.
+ *
+ * @param string          $excerpt
+ * @param object \WP_Post $post
+ * @return string|null
+ */
+function post_excerpt( $excerpt, \WP_Post $post ): ?string {
+	if ( 'book' === get_post_type( $post->ID ) ) {
+		return $post->post_excerpt;
+	}
+	return $excerpt;
+}
+// add_filter( 'the_excerpt', __NAMESPACE__ . '\post_excerpt' );
+add_filter( 'get_the_excerpt', __NAMESPACE__ . '\post_excerpt', 10, 2 );
