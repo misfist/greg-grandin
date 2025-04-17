26c202349288bafc8ea8b04df5a04a9f5ccffa09
pea@misfist.com <pea@PEA-Lutzs-Mac-Mini.local>
2025-04-16 20:31:23 -0400

    Helpers

--- inc/functions.php
+++ inc/functions.php
@@ -35,3 +35,76 @@ function render_subtitle() {
 		echo $subtitle;
 	}
 }
+
+/**
+ * Display blockquote from post
+ *
+ * @link https://www.billerickson.net/access-gutenberg-block-data/
+ */
+
+ /**
+ * Get book quotes
+ *
+ * @return void
+ */
+function get_blockquotes( $post_id = null ) {
+	global $post;
+	$post_id = ( $post_id ) ? $post_id : $post->ID;
+	$block_name = 'core/quote';
+
+	if( ! has_block( $block_name, $post_id ) ) {
+		return;
+	}
+	
+	$blocks = parse_blocks( $post->post_content );
+	$max    = 2;
+	$count  = 0;
+
+	$output = '';
+
+	foreach ( $blocks as $block ) {
+		if ( 'core/quote' === $block['blockName'] ) {
+			if ( $max < $count ) {
+				break;
+			}
+			// error_log( $block['blockName'] . '' . $count );
+			$output .= render_block( $block );
+			++$count;
+		}
+	}
+	return $output;
+}
+
+
+/**
+ * Display book quotes
+ *
+ * @return void
+ */
+function render_blockquotes( $post_id = null ) {
+	global $post;
+	$post_id = ( $post_id ) ? $post_id : $post->ID;
+
+	echo get_blockquotes( $post_id );
+	// $block_name = 'core/quote';
+
+	// if( ! has_block( $block_name, $post_id ) ) {
+	// 	return;
+	// }
+	
+	// $blocks = parse_blocks( $post->post_content );
+	// $max    = 2;
+	// $count  = 0;
+
+	// foreach ( $blocks as $block ) {
+	// 	if ( 'core/quote' === $block['blockName'] ) {
+	// 		if ( $max < $count ) {
+	// 			break;
+	// 		}
+	// 		// error_log( $block['blockName'] . '' . $count );
+	// 		echo render_block( $block );
+	// 		++$count;
+	// 	}
+	// }
+	// // return;
+}
