650b23df6f413524f9d082eaf2b7256d8fe4c6ca
pea@misfist.com <pea@PEA-Lutzs-Mac-Mini.local>
2025-04-21 02:51:25 -0400

    Set up image sizes

--- inc/setup.php
+++ inc/setup.php
@@ -18,5 +18,21 @@ namespace Greg_Grandin;
  */
 function post_format_setup() {
 	add_theme_support( 'post-formats', array( 'audio', 'gallery', 'image', 'link', 'quote', 'video' ) );
+
+	update_option( 'thumbnail_size_w', 150 );
+	update_option( 'thumbnail_size_h', 225 );
+	update_option( 'thumbnail_crop', 1 );
+
+	update_option( 'medium_size_w', 400 );
+	update_option( 'medium_size_h', 600 );
+	update_option( 'medium_crop', 1 );
+
+	update_option( 'medium_large_size_w', 500 );
+	update_option( 'medium_large_size_h', 750 );
+	update_option( 'medium_large_crop', 1 );
+
+	update_option( 'large_size_w', 600 );
+	update_option( 'large_size_h', 900 );
+	update_option( 'large_crop', 1 );
 }
 add_action( 'after_setup_theme', __NAMESPACE__ . '\post_format_setup' );
